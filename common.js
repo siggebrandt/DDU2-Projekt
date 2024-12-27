61a6a445c0dcf4371d8c4417d9e820c1f459a4af
siggebrandt <sigge.brandt567@gmail.com>
2024-12-27 13:24:20 +0100

    funktion för att skriva ut arrayen av siffror

--- common.js
+++ common.js
@@ -17,23 +17,32 @@ function randomNumber() {
 let arrayOfRandomNumbers;
 
 function generateNumbers() {
-    document.querySelector("#createNumbers").innerHTML = `<label>How many numbers in the grid?</label>` +
+    document.querySelector("#createNumbers").innerHTML =
+        `<label>How many numbers in the grid?</label>` +
         `<input type="text" id="howManyNumbers" size="4">` +
         `<button id="generateButton">Create</button>`;
+
+    let gridOfNumbers = document.querySelector("#gridOfNumbers");
 
     document.querySelector("#generateButton").addEventListener("click", function (e) {
         let numberToGenerate = Number(document.querySelector("input#howManyNumbers").value);
         arrayOfRandomNumbers = [];
+        gridOfNumbers.innerHTML = "";
 
         while (arrayOfRandomNumbers.length < numberToGenerate) {
             arrayOfRandomNumbers.push(randomNumber());
         }
         console.log(arrayOfRandomNumbers);
+
+        for (number of arrayOfRandomNumbers) {
+            let numberBox = document.createElement("div");
+            numberBox.classList.add("number");
+            numberBox.textContent = number;
+            gridOfNumbers.appendChild(numberBox);
+        }
     });
 }
 
-//function
 
 
 generateNumbers();
-
