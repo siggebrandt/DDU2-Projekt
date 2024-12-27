0631b4bf6b6073a298b01d0f20660f31fc6077ef
siggebrandt <sigge.brandt567@gmail.com>
2024-12-27 12:46:26 +0100

    skapat funktion för att generera random siffror

--- common.js
+++ common.js
@@ -10,10 +10,30 @@ if (homeLink) {
     homeLink.appendChild(createHomeLink());
 }
 
-
-
-function randomNumber(max) {
-    return Math.floor(Math.random() * max);
+function randomNumber() {
+    return Math.floor(Math.random() * 99);
 }
 
-console.log(randomNumber(100))
+let arrayOfRandomNumbers;
+
+function generateNumbers() {
+    document.querySelector("#createNumbers").innerHTML = `<label>How many numbers in the grid?</label>` +
+        `<input type="text" id="howManyNumbers" size="4">` +
+        `<button id="generateButton">Create</button>`;
+
+    document.querySelector("#generateButton").addEventListener("click", function (e) {
+        let numberToGenerate = Number(document.querySelector("input#howManyNumbers").value);
+        arrayOfRandomNumbers = [];
+
+        while (arrayOfRandomNumbers.length < numberToGenerate) {
+            arrayOfRandomNumbers.push(randomNumber());
+        }
+        console.log(arrayOfRandomNumbers);
+    });
+}
+
+//function
+
+
+generateNumbers();
+
